b077e5acc7b283e11542df7ef4a5d3788162d5bd
Paresh Ranjan Rout <104611685+PARESHRANJAN299@users.noreply.github.com>
2025-07-28 23:40:13 +0530

    SQL Day -1 Challs.. ''Fun game''

--- p_day-1 pratical.sql
+++ p_day-1 pratical.sql
@@ -1,12 +1,8 @@
-show databases
+show databases;
 use student_info;
-show tables 
 
-select * from student_id_cards
-select * from students;
-
+show tables;
 -----✅Step1:CreatetheTable
-
 CREATE TABLE products (
     id INT PRIMARY KEY,
     name VARCHAR(100),
@@ -14,7 +10,6 @@ CREATE TABLE products (
     price INT,
     created_at DATE
 );
-
 ----InsertSampleData
 INSERT INTO products (id, name, category, price, created_at) VALUES
 (1, 'Pro Mouse', 'Accessories', 500, '2024-01-01'),
@@ -23,37 +18,49 @@ INSERT INTO products (id, name, category, price, created_at) VALUES
 (4, 'Laptop Pro', 'Laptop', 50000, '2024-03-01'),
 (5, 'Laptop Basic', 'Laptop', 30000, '2023-12-15');
 
-------------------------Queries
-check how many database and tables available in our production
-show databases
-show tables
 
---in my case i have one database. if in your case there are multiple databases are there so you have to change the database name use below systax.
---use database_name
---Showall products
-select * from products;
---2. Show name and price only
-select price from products;
---3. Products priced above500
-
-SELECT price FROM products
-WHERE price >= 500;
-
-select price
-from products
-where price  > 500;
-
------While writing or debugging a SQL query, it's common to pause and check a table's structure — like its columns, data types, and constraints.
-DESCRIBE products;
-SHOW CREATE TABLE products;
-
---Products containing “Pro” in the name
 select * from products
-----
+
+select
+name, price from products
+select name from products
+
+select name, price
+from products 
+where price >= 500
+
 select name
 from products
-where name  like '%pro%';
+where name like '%pro%'
 
-select created_at
+select * from products
+
+select name, created_at
 from products
-where created_at between '2024-01-01' and '2024-12-01';
+where created_at between '2024-01-01' and '2024-12-30'
+
+select * 
+from products
+
+select DISTINCT (category) 
+from products
+
+select name, price 
+from products
+where price between 300  and 1000;
+
+select *
+from products
+
+select category
+from products
+where category = 'laptop';
+
+select name 
+from products
+where name like 'laptop%';
+
+select name
+from products 
+order by name asc;
+
