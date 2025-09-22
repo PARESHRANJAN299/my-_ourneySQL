4826349133fa7c14dc97dfa001ac99733d43281b
Paresh Ranjan Rout <104611685+PARESHRANJAN299@users.noreply.github.com>
2025-09-23 03:37:16 +0530

    Add files via upload

--- CASE  logic.sql
+++ CASE  logic.sql
@@ -6,8 +6,32 @@ account_type,
 balance,
 CASE 
 WHEN balance > 1000000 THEN 'higher rich'
-WHEN balance between 50000 and 1000000 THEN 'medium'
+WHEN balance between 500000 and 100000 THEN 'medium'
 else 'Small'
 END as category_balance
 from 
 accounts
+
+1. Categorize Balance as High/Medium/Low
+select *,
+CASE 
+WHEN balance > 100000  THEN 'HIGH'
+WHEN balance between 50000 AND 100000 THEN 'Medium'
+ELSE 'Low'
+END as Categorize_Balance
+from accounts;
+2. Rename Account Types into Short Codes
+select * , 
+CASE account_type
+WHEN 'Savings' THEN 'SAV'
+WHEN 'CURRENT' THEN 'CUR'
+WHEN 'Fixed Deposit' THEN 'FD'
+ELSE 'OTHER'
+END RenameACC
+FROM ACCOUNTS;
+
+3. Check if Customer Has More Than 1 Account
+
+select * , 
+case
+when customer_id > 1 then 
